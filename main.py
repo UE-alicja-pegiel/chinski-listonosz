bafb28ec8cc845740c7c8ba05c31fc99e9aee721
Alicja Pęgiel <alicja.pegiel@edu.uekat.pl>
2022-12-13 12:53:49 +0100

    Visualization

--- main.py
+++ main.py
@@ -7,20 +7,38 @@ graph.generate_graph()
 graph.print_edges()
 #graph.print_degrees()
 
-plt.figure(figsize=[15, 15])
-plt.title('Graf')
-plt.xlabel('współrzędna x')
-plt.ylabel('współrzędna y')
+plt.figure(figsize=[10, 10])
+plt.suptitle('\nGraphs\n')
 
 for edge in graph.edges:
+    plt.subplot(2, 2, 1)
+    plt.title('\nGraph\n')
+    plt.xlabel('x coordinate')
+    plt.ylabel('y coordinate')
     x = [edge.head.x, edge.tail.x]
     y = [edge.head.y, edge.tail.y]
-    plt.plot(x, y, 'o--r', ms=20, alpha=.5)
+    plt.plot(x, y, 'o--r', ms=10, alpha=.5)
     plt.xticks([0, 25, 50, 75, 100])
     plt.yticks([0, 25, 50, 75, 100])
     plt.annotate(edge.head.idn, (x[0], y[0]))
     plt.annotate(edge.tail.idn, (x[1], y[1]))
     plt.text((x[0] + x[1]) / 2, (y[0] + y[1]) / 2, edge.cost)
 
-plt.show()
+alpha = 0.1
 
+for edge in graph.edges:
+    plt.subplot(2, 2, 2)
+    plt.title('\nEulerian path\n')
+    plt.xlabel('x coordinate')
+    plt.ylabel('y coordinate')
+    x = [edge.head.x, edge.tail.x]
+    y = [edge.head.y, edge.tail.y]
+    plt.plot(x, y, 'o--g', ms=10, alpha=alpha)
+    plt.xticks([0, 25, 50, 75, 100])
+    plt.yticks([0, 25, 50, 75, 100])
+    plt.annotate(edge.head.idn, (x[0], y[0]))
+    plt.annotate(edge.tail.idn, (x[1], y[1]))
+    plt.text((x[0] + x[1]) / 2, (y[0] + y[1]) / 2, edge.cost)
+    alpha += 0.05
+
+plt.show()
